eecb0be96034b76c52b4ae924b416897f3585aef
123UsernamemanresU321 <erichuang.shangjing@outlook.com>
2026-04-07 10:31:31 +0200

    Refactor invite handling and persistence logic

--- cogs/invites.py
+++ cogs/invites.py
@@ -7,16 +7,29 @@ from discord.ext import commands
 from cogs import config as cfg
 
 Cog = commands.Cog
-invites = {"SxCN3xDDHs"}
+invites = {}
+
+PERMANENT_INVITES = {"SxCN3xDDHs"}
 
 
 class Invites(Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
-        if os.path.exists("data/invites.p"):
-            temp = pickle.load(open("data/invites.p", "rb"))
-            for invite in temp:
-                invites[invite] = temp[invite]
+        global invites
+        invites = {}
+
+        path = "data/invites.p"
+        if os.path.exists(path):
+            with open(path, "rb") as f:
+                temp = pickle.load(f)
+
+            if isinstance(temp, dict):
+                invites.update(temp)
+            elif isinstance(temp, set):
+                invites.update({code: 0 for code in temp})
+
+        for code in PERMANENT_INVITES:
+            invites.setdefault(code, 0)
 
     @commands.is_owner()
     @commands.command(aliases=["ui"])
@@ -24,38 +37,44 @@ class Invites(Cog):
         inv_list = await ctx.guild.invites()
         for invite in inv_list:
             invites[invite.code] = invite.uses
-        pickle.dump(invites, open("data/invites.p", "wb+"))
-        print(invites)
+
+        for code in PERMANENT_INVITES:
+            invites.setdefault(code, 0)
+
+        with open("data/invites.p", "wb") as f:
+            pickle.dump(invites, f)
+
+        await ctx.send("Invites updated.")
 
     @Cog.listener()
     async def on_invite_create(self, invite: discord.Invite):
-        invites[invite.code] = 0
-        pickle.dump(invites, open("data/invites.p", "wb+"))
+        invites[invite.code] = invite.uses or 0
+        with open("data/invites.p", "wb") as f:
+            pickle.dump(invites, f)
 
     @Cog.listener()
     async def on_member_join(self, member: discord.Member):
         temp_invites = await member.guild.invites()
 
-        # Check possible places they could have joined from
         possible_joins = set()
         for invite in temp_invites:
-            if invite.uses > invites[invite.code]:
+            old_uses = invites.get(invite.code, 0)
+            if invite.uses > old_uses:
                 possible_joins.add(invite)
             invites[invite.code] = invite.uses
+
+        with open("data/invites.p", "wb") as f:
+            pickle.dump(invites, f)
+
         possible_string = " ".join(
-            [f"{invite.code} by {invite.inviter.mention}" for invite in possible_joins]
+            f"{invite.code} by {invite.inviter.mention}" for invite in possible_joins
         )
+
         embed = discord.Embed()
         embed.add_field(name="User Joined", value=member.mention, inline=False)
         embed.add_field(
             name="Possible Invites",
-            value=(
-                (
-                    possible_string
-                    if possible_string != ""
-                    else "None (probably Discovery)"
-                ),
-            ),
+            value=possible_string if possible_string else "None (probably Discovery)",
             inline=False,
         )
 
@@ -66,13 +85,14 @@ class Invites(Cog):
             )
         else:
             embed.add_field(
-                name="Recently Created", value=f"{join_delta} seconds ago", inline=False
+                name="Recently Created",
+                value=f"{join_delta} seconds ago",
+                inline=False,
             )
             await self.bot.get_channel(cfg.Config.config["warn_channel"]).send(
                 embed=embed
             )
 
-        # send welcome message
         welcome_message = (
             f"Welcome to the Mathematical Olympiads Discord Server "
             f"<@!{member.id}>! Introduce yourself in "
@@ -85,5 +105,4 @@ class Invites(Cog):
 
 
 async def setup(bot: commands.Bot):
-    i = Invites(bot)
-    await bot.add_cog(i)
+    await bot.add_cog(Invites(bot))
